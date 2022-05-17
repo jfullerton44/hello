f84a6d83a357c87d93a80be3d4b8733913635d79
Jake Fullerton <jfullerton@microsoft.com>
2022-05-17 14:26:36 -0700

    add new func

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/jfullerton44/hello
 
 go 1.17
+
+require github.com/microsoft/dev-tunnels v0.0.2
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/rodaine/table v1.0.1 // indirect
+	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+)
--- hello.go
+++ hello.go
@@ -1,6 +1,10 @@
 package hello
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/microsoft/dev-tunnels/go/tunnels"
+)
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
@@ -13,4 +17,12 @@ func Hi(name string) string {
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
+}
+
+func Create(name string) string {
+	// Return a greeting that embeds the name in a message.
+	tunnel := tunnels.Tunnel{
+		Name: name,
+	}
+	return tunnel.Name
 }
